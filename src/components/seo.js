cc4457c21d16e4e8c1490ac4b86fd2635d4130ea
Vedant2355 <88609765+Ved235@users.noreply.github.com>
2022-06-21 23:12:31 +0530

    Optimization

--- src/components/seo.js
+++ src/components/seo.js
@@ -2,10 +2,10 @@ import React from "react";
 import Head from "next/head";
 
 export default function SEO({
-  description = "startup landing descriptions",
-  author = "RedQ, Inc",
+  description = "Obfuscate Code",
+  author = "Vedant and Omar",
   meta,
-  title = "startup landing title",
+  title = "CodeGuard",
 }) {
   const metaData = [
     {
