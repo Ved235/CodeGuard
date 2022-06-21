55bb0c6e99e1fef62061d028b29370310f613f85
Omar <omar.moustafa@toolsandapps4us.site>
2022-06-21 11:37:32 +0400

    Add API to JS

--- src/components/form.js
+++ src/components/form.js
@@ -86,21 +86,7 @@ function Error(){
               if (page === FormTitles.length - 1) {
                 if (formData.programmingLanguage === "JavaScript") {
                   Alert()
-                  fetch(formData.codeLink)
-                  .then(resp => resp.blob())
-                  .then(blob => {
-                    const url = window.URL.createObjectURL(blob);
-                    const a = document.createElement('a');
-                    a.style.display = 'none';
-                    a.href = url;
-                    // the filename you want
-                    a.download = 'code.js';
-                    document.body.appendChild(a);
-                    a.click();
-                    window.URL.revokeObjectURL(url);
-                    
-
-                  });
+                  window.location = "https://api.codeguard.tech?link=" + formData.codeLink + "&lang=jsy";                    
                 }
                 
               else if(formData.programmingLanguage === "Python"){
